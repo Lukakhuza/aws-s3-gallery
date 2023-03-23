8d6ac359c1c3696e952817232d0a714a1640be50
Lukakhuza <78911198+Lukakhuza@users.noreply.github.com>
2023-03-23 17:54:47 -0400

    image upload functionality partially done

---
+++ src/main/java/com/example/gallery/profile/UserProfileService.java
@@ -0,0 +1,29 @@
+package com.example.gallery.profile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserProfileService {
+
+    private final UserProfileDataAccessService userProfileDataAccessService;
+
+    @Autowired
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+        this.userProfileDataAccessService = userProfileDataAccessService;
+    }
+
+    List<UserProfile> getUserProfile() {
+        return userProfileDataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        if(file.isEmpty()) {
+            throw new IllegalStateException("file is empty");
+        }
+    }
+}
