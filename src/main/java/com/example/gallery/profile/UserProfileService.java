a18af3bb76b4fca0c28d810226f70592180a15ce
Lukakhuza <78911198+Lukakhuza@users.noreply.github.com>
2023-04-06 16:04:13 -0400

    frontend and other updates

--- src/main/java/com/example/gallery/profile/UserProfileService.java
+++ src/main/java/com/example/gallery/profile/UserProfileService.java
@@ -1,11 +1,15 @@
 package com.example.gallery.profile;
 
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+
+import static org.apache.http.entity.ContentType.*;
 
 @Service
 public class UserProfileService {
@@ -25,5 +29,12 @@ public class UserProfileService {
         if(file.isEmpty()) {
             throw new IllegalStateException("file is empty");
         }
+
+        if (!Arrays.asList(IMAGE_JPEG,IMAGE_PNG,IMAGE_GIF).contains(file.getContentType())) {
+            throw new IllegalStateException("file must be an image");
+        }
+
+
+
     }
 }
